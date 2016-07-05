9ef737801be33f52d3c8124c1ea116380737c88f
László Kiss <laszlo.kiss1@ge.com>
2016-07-05 15:18:12 +0200

    Merge

--- app.js
+++ app.js
@@ -13,5 +13,25 @@ angular.module("myapp", ["contactServiceModule", "authServiceModule"])
                 console.log(data)});
     });
 
+    var result = contactService.getContact();
+    console.log("getContact: ", result);
+
+    var result = contactService.getContactByExample();
+    console.log("getContactByExample: ", result);
+
+    var result = contactService.getContactsInGroup();
+    console.log("getContactsInGroup: ", result);
+
+    var result = contactService.addContactToGroup();
+    console.log("addContactToGroup: ", result);
+
+    var result = contactService.updateContact();
+    console.log("updateContact: ", result);
+
+    var result = contactService.deleteContact();
+    console.log("deleteContact: ", result);
+
+    var result = contactService.validateContact();
+    console.log("validateContact: ", result);
 
 });
