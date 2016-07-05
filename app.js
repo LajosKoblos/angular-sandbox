22bbc52a8dc572888e619429babd34987bb1c2aa
Lajos Köblös <lajos.koblos@ge.com>
2016-07-05 12:28:08 +0200

    Add ContactService

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-require("authService");
 require("contactService");
+require("authService");
 
 angular.module("myapp", ["contactServiceModule"])
 
 .controller("appCtrl", function(contactService){
-    contactService.write2Console();
+    var result = contactService.getContact();
+    console.log("getContact: ", result);
+
+    var result = contactService.getContactByExample();
+    console.log("getContactByExample: ", result);
+
+    var result = contactService.getContactsInGroup();
+    console.log("getContactsInGroup: ", result);
+
+    var result = contactService.addContactToGroup();
+    console.log("addContactToGroup: ", result);
+
+    var result = contactService.updateContact();
+    console.log("updateContact: ", result);
+
+    var result = contactService.deleteContact();
+    console.log("deleteContact: ", result);
+
+    var result = contactService.validateContact();
+    console.log("validateContact: ", result);
 });
